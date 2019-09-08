4f01a114479aa2158cb18be336cdf70c89888ba2
Ankit Pati <contact@ankitpati.in>
2019-09-08 11:20:42 +0530

    remove unnecessary code

--- src/vcfex.c
+++ src/vcfex.c
@@ -1,40 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *fgetstr(FILE * fp, int delim)
-{
-    int c;
-    size_t n;
-    char *s;
-
-    if (!(s = malloc(sizeof(*s)))) return NULL;
-
-    for (n = 0; (c = fgetc(fp)) != EOF && c != delim; s[n++] = c)
-        if (!(s = realloc(s, sizeof(*s) * (n + 2)))) return NULL;
-
-    s[n] = '\0';
-    return s;
-}
-
 int main(int argc, char **argv)
 {
-    int c;
+    int c, argi;
     size_t i;
     char *vname;
-    FILE *fl, *fv;        /* fl: list file; fv: vcf file */
+    FILE *fv;
 
-    if (argc != 2) goto argerr;
+    if (argc < 2) goto argerr;
 
-    if (!(fl = fopen(argv[1], "r"))) goto ioerr;
-
-    while ((vname = fgetstr(fl, '\n'))) {
-        if (feof(fl)) {
-            free(vname);
-            break;
-        }
+    for (argi = 1; argi < argc; ++argi) {
+        vname = argv[argi];
 
         fv = fopen(vname, "rb");
-        free(vname);
         if (!fv) goto ioerr;
 
         for (i = 0; i < 4; c == ';' ? ++i : 0) {
@@ -78,15 +57,13 @@ int main(int argc, char **argv)
         fclose(fv);
     }
 
-    fclose(fl);
     return 0;
 
 argerr:
-    fprintf(stderr, "Usage:\n\tvcfex filelist.lst\n");
+    fprintf(stderr, "Usage:\n\tvcfex <file>...\n");
     return 2;
 
 ioerr:
-    if (fl) fclose(fl);
     fprintf(stderr, "vcfex: File could not be opened!\n");
     return 3;
 }
